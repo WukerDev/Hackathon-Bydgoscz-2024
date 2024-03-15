d0d402b8b74bf8670fc620d344bb502614ff9e8f
TroubleMakerxx <sebek4949@gmail.com>
2024-03-15 23:06:14 +0100

    SM notification work

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -6,4 +6,50 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "react-native-push-notification";
+import { Platform } from 'react-native';
+
+PushNotification.configure({
+  onRegister: function (token) {
+    console.log("TOKEN:", token);
+  },
+
+  onNotification: function (notification) {
+    console.log("NOTIFICATION:", notification);
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+
+  onAction: function (notification) {
+    console.log("ACTION:", notification.action);
+    console.log("NOTIFICATION:", notification);
+  },
+
+  onRegistrationError: function(err) {
+    console.error(err.message, err);
+  },
+
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  popInitialNotification: true,
+  requestPermissions: Platform.OS === 'ios',
+});
+
+PushNotification.createChannel(
+    {
+      channelId: 'your_channel_id', 
+      channelName: 'Your Channel Name', 
+      channelDescription: 'Your Channel Description', 
+      soundName: 'default', 
+      importance: 4, 
+      vibrate: true, 
+    },
+    (created) => console.log(`Channel created: ${created}`)
+  );
+
+
 AppRegistry.registerComponent(appName, () => App);
