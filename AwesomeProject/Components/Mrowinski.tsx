7e5d15d546f3ec7a98a611b051fe067f93e73bd2
TroubleMakerxx <sebek4949@gmail.com>
2024-03-16 01:46:33 +0100

    Notification Simple

--- AwesomeProject/Components/Mrowinski.tsx
+++ AwesomeProject/Components/Mrowinski.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, TextInput, Alert, PermissionsAndroid } from 'react-native';
-import { showNotification, handleScheduledNotification, handleCancel, alramNotification } from '../src/notification';
+import { showNotification } from '../src/notification';
 import tw from 'twrnc';
 import { Platform } from 'react-native';
 
@@ -35,30 +35,6 @@ const requestNotificationPermission = async () => {
 function Mrowinski() {
   const [selectedTime, setSelectedTime] = useState<string>('');
   requestNotificationPermission();
-  const handleSetNotification = () => {
-    if (selectedTime) {
-      const [hours, minutes] = selectedTime.split(':').map(Number);
-      const currentDate = new Date(Date.now());
-
-      const notificationDate = new Date(
-        currentDate.getFullYear(),
-        currentDate.getMonth(),
-        currentDate.getDate(),
-        hours,
-        minutes,
-        0 
-      );
-
-      if (notificationDate > currentDate) {
-        notificationDate.setDate(notificationDate.getDate() + 1);
-      }
-
-      alramNotification('Hello', 'This is a notification at ' + selectedTime, notificationDate);
-      Alert.alert('Notification set for ' + notificationDate + '.');
-    } else {
-      Alert.alert('Please select a time for the notification.');
-    }
-  };
 
   return (
     <View style={[tw`flex-1`]}>
@@ -69,34 +45,6 @@ function Mrowinski() {
         <Text style={tw`text-white`}>Schedule Notification</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity
-        style={[tw`bg-green-500 rounded-md p-3 m-2`, { alignSelf: 'center' }]}
-        onPress={() => handleScheduledNotification('Hello', 'Notification after 5 seconds')}
-      >
-        <Text style={tw`text-white`}>Schedule Notification</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={[tw`bg-red-500 rounded-md p-3 m-2`, { alignSelf: 'center' }]}
-        onPress={() => handleCancel()}
-      >
-        <Text style={tw`text-white`}>Cancel Notification</Text>
-      </TouchableOpacity>
-
-      <TextInput
-        style={[tw`border rounded-md p-2 m-2`, { alignSelf: 'center' }]}
-        placeholder="Enter time (HH:MM)"
-        value={selectedTime}
-        onChangeText={(text) => setSelectedTime(text)}
-        keyboardType="numeric"
-      />
-
-      <TouchableOpacity
-        style={[tw`bg-blue-500 rounded-md p-3 m-2`, { alignSelf: 'center' }]}
-        onPress={handleSetNotification}
-      >
-        <Text style={tw`text-white`}>Set Notification</Text>
-      </TouchableOpacity>
     </View>
   );
 }
