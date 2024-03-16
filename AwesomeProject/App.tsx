7660805cba410de722596202a3717d2d1cd1d51c
Jakub Kordella <kubusmateusz1@gmail.com>
2024-03-16 04:38:33 +0100

    Add files via upload

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,28 +1,55 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer} from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
+import HomeScreen from './Components/HomeScreen';
+import Settings from './Components/Settings';
+import Messages from './Components/Messages';
 import Kozakowski from './Components/Kozakowski';
 import Jacoszek from './Components/Jacoszek';
 import Kordella from './Components/Kordella';
 import Switalski from './Components/Switalski';
 import Zelazkiewicz from './Components/Zelazkiewicz';
 import Mrowinski from './Components/Mrowinski';
-import HomeScreen from './Components/HomeScreen';
 
-const Stack = createNativeStackNavigator();
+import { CustomDarkTheme, CustomLightTheme } from './Components/Theme'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+
+const Tab = createBottomTabNavigator();
 
 function App() {
+  const scheme = useColorScheme();
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }} />
-        <Stack.Screen name="Kozakowski" component={Kozakowski} />
-        <Stack.Screen name="Jacoszek" component={Jacoszek} />
-        <Stack.Screen name="Kordella" component={Kordella} />
-        <Stack.Screen name="Switalski" component={Switalski} />
-        <Stack.Screen name="Zelazkiewicz" component={Zelazkiewicz} />
-        <Stack.Screen name="Mrowinski" component={Mrowinski} />
-      </Stack.Navigator>
+    <NavigationContainer theme={scheme === 'dark' ? CustomDarkTheme : CustomLightTheme}>
+      <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="home" color={color} size={size} />
+            ),
+          }} 
+        />
+         <Tab.Screen name="Messages" component={Messages} options={{ title: 'Messages',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="message" color={color} size={size} />
+            ), }} />
+        <Tab.Screen name="Options" component={Settings} options={{ title: 'Settings',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="settings" color={color} size={size} />
+            ), }} />
+        <Tab.Screen name="Kozakowski" component={Kozakowski} />
+        <Tab.Screen name="Jacoszek" component={Jacoszek} />
+        <Tab.Screen name="Kordella" component={Kordella} />
+        <Tab.Screen name="Switalski" component={Switalski} />
+        <Tab.Screen name="Zelazkiewicz" component={Zelazkiewicz} />
+        <Tab.Screen name="Mrowinski" component={Mrowinski} />
+
+
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
