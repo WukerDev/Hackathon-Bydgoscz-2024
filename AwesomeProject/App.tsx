26d7e68831942c7be64f2c8d06cf01ded81a4abb
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2024-03-16 01:39:24 +0100

    Debug ikon

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -4,6 +4,13 @@ import { useColorScheme } from 'react-native';
 import HomeScreen from './Components/HomeScreen';
 import Settings from './Components/Settings';
 import Messages from './Components/Messages';
+import Kozakowski from './Components/Kozakowski';
+import Jacoszek from './Components/Jacoszek';
+import Kordella from './Components/Kordella';
+import Switalski from './Components/Switalski';
+import Zelazkiewicz from './Components/Zelazkiewicz';
+import Mrowinski from './Components/Mrowinski';
+
 import { CustomDarkTheme, CustomLightTheme } from './Components/Theme'; 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
@@ -34,6 +41,14 @@ function App() {
             tabBarIcon: ({ color, size }) => (
               <MaterialIcons name="settings" color={color} size={size} />
             ), }} />
+        <Tab.Screen name="Kozakowski" component={Kozakowski} />
+        <Tab.Screen name="Jacoszek" component={Jacoszek} />
+        <Tab.Screen name="Kordella" component={Kordella} />
+        <Tab.Screen name="Switalski" component={Switalski} />
+        <Tab.Screen name="Zelazkiewicz" component={Zelazkiewicz} />
+        <Tab.Screen name="Mrowinski" component={Mrowinski} />
+        
+
       </Tab.Navigator>
     </NavigationContainer>
   );
