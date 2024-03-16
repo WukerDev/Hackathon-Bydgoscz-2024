07c10baf47039215bcdcc864f8266c696db84bbb
saulgoodman20 <kubusmateusz1@gmail.com>
2024-03-16 06:05:13 +0100

    Update theme colors and add settings stack screen

---
+++ AwesomeProject/Components/Settings/Pages/Account/Logout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Logout: React.FC = () => {
+    return (
+        <View>
+            <Text>Logout Page</Text>
+        </View>
+    );
+};
+
+export default Logout;
---
+++ AwesomeProject/Components/Settings/Pages/Account/Security.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Security: React.FC = () => {
+    return (
+        <View>
+            <Text>Security Page</Text>
+            <Text>This is the security page content.</Text>
+            {/* Your additional code here */}
+        </View>
+    );
+};
+
+export default Security;
