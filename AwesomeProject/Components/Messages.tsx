30267e4ecbcd0e3d365253bfa71462cc3a9c2a55
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2024-03-16 05:03:02 +0100

    Zmiana

--- AwesomeProject/Components/Messages.tsx
+++ AwesomeProject/Components/Messages.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import tw from 'twrnc';
+import Switalski from './Switalski';
 
 function Messages() {
   return (
-    <View style={tw`pt-6 bg-blue-100`}>
-      <Text>Component 1</Text>
-    </View>
+    <Switalski/>
   );
 }
 
